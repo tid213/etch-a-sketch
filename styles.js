cf3e3845c37e62bea3da01ed0a80e7060352d791
omgnoez <tid2136141@gmail.com>
2021-12-20 16:20:07 -0700

    created container for boxes, created function to generate16x16 grid, created function to randomly generate a color

---
+++ styles.js
@@ -0,0 +1,17 @@
+function makeRows(rows, cols){
+    var container = document.getElementById("container");
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+    for (i=0; i<(rows * cols); i++){
+        var cell = document.createElement("div");
+        cell.innerText = (i + 1);
+        container.appendChild(cell).className = "grid-item";
+    }
+}
+
+function generateColor(){
+    let choices = ['blue', 'red', 'yellow', 'green', 'purple', 'orange'];
+    let randomNum = Math.floor(Math.random()*choices.length);
+    let randomColor =  choices[randomNum];
+    return randomColor;
+}
